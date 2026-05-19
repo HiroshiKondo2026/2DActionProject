8867f4a265ce6035db0444414b6eb5ff4a6aced4
HiroshiKondo2026 <uni202601_04@telework.to>
2026-05-19 15:50:56 +0900

    Enemyの攻撃範囲調整
    大きすぎてEnemy側過ぎたので密着しないとヒットしてなかった

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -19,6 +19,34 @@ public class EnemyAI : MonoBehaviour
     [SerializeField]
     private float chaseDistance = 5f;
 
+    // 攻撃開始距離
+    [SerializeField]
+    private float attackDistance = 1.5f;
+
+    // 次攻撃までの間隔
+    [SerializeField]
+    private float attackCooldown = 2f;
+
+    // 次攻撃可能時間
+    private float nextAttackTime;
+
+    // 攻撃判定位置
+    [SerializeField]
+    private Transform attackPoint;
+
+    // 攻撃範囲
+    [SerializeField]
+    private float attackRadius = 0.8f;
+
+    // PlayerLayer
+    [SerializeField]
+    private LayerMask playerLayer;
+
+    // 攻撃力
+    [SerializeField]
+    private int attackDamage = 1;
+
+
     // 初期化
     private void Awake()
     {
@@ -38,14 +66,35 @@ public class EnemyAI : MonoBehaviour
         // 移動方向
         float moveDirection;
 
+        // 攻撃距離内なら停止
+        if (distance <= attackDistance)
+        {
+            // 移動停止
+            rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
+
+            // 攻撃可能なら攻撃
+            if (Time.time >= nextAttackTime) Attack();
+
+            return;
+        }
+
         // Playerが近いなら追尾
         if (distance <= chaseDistance)
         {
-            // Playerが右にいるか
+            // Playerが右にいるか　Playerが右なら1　左なら - 1
             moveDirection = playerTransform.position.x > transform.position.x ? 1f : -1f;
 
-            // Playerに合わせて向きを更新
-            isFacingRight = moveDirection > 0;
+            // Playerが右にいるか
+            float targetDirection = playerTransform.position.x > transform.position.x ? 1f : -1f;
+
+            // 向き違うなら反転
+            if ((targetDirection > 0 && !isFacingRight) || (targetDirection < 0 && isFacingRight))
+            {
+                Flip();
+            }
+
+            // 移動方向
+            moveDirection = isFacingRight ? 1f : -1f;
         }
         else
         {
@@ -68,7 +117,6 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
-    // Flip:
     // 向き変更
     private void Flip()
     {
@@ -83,5 +131,48 @@ public class EnemyAI : MonoBehaviour
 
         // Scale反映
         transform.localScale = scale;
+    }
+
+    // 攻撃処理
+    private void Attack()
+    {
+
+
+
+        // 攻撃範囲内Player取得 円範囲内のCollider全部取得
+        Collider2D[] hitPlayers = Physics2D.OverlapCircleAll(attackPoint.position, attackRadius, playerLayer);
+
+        Debug.Log("Hitのカウント距離 : " + hitPlayers.Length);
+        Debug.Log("Enemy Attack!");
+
+        // 全Playerへ処理
+        foreach (Collider2D player in hitPlayers)
+        {
+            // PlayerHealth取得
+            PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+
+            // PlayerHealthあるなら
+            if (playerHealth != null)
+            {
+                // ダメージ
+                playerHealth.TakeDamage(attackDamage, transform.position);
+            }
+        }
+
+        // 次回攻撃時間
+        nextAttackTime = Time.time + attackCooldown;
+    }
+
+    // Gizmo表示
+    private void OnDrawGizmos()
+    {
+        // AttackPoint未設定なら終了
+        if (attackPoint == null) return;
+
+        // 表示色は赤
+        Gizmos.color = Color.red;
+
+        // 攻撃範囲表示
+        Gizmos.DrawWireSphere(attackPoint.position, attackRadius);
     }
 }
