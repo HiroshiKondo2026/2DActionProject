c8779dcb9056d361eaad081a9ab6d2889bb63bfc
HiroshiKondo2026 <uni202601_04@telework.to>
2026-05-19 13:08:26 +0900

    被ダメージで無敵時間を挟むようにした

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -9,6 +9,13 @@ public class PlayerHealth : MonoBehaviour
     // 現在HP
     private int currentHP;
 
+    // 無敵状態の有無
+    private bool isInvincible = false;
+
+    // 無敵時間用変数
+    [SerializeField]
+    private float invincibleTime = 1.0f;
+
     // ゲーム開始時に呼ばれる
     private void Start()
     {
@@ -21,11 +28,20 @@ public class PlayerHealth : MonoBehaviour
     // ダメージを受ける処理
     public void TakeDamage(int damage)
     {
+        // 無敵中ならダメージ無効
+        if (isInvincible)
+        {
+            Debug.Log("無敵中だぜ");
+
+            return;
+        }
         // HPをダメージで減少させる
         currentHP -= damage;
 
         Debug.Log("Playerは" + damage + " のダメージを受けた");
         Debug.Log("現在HP: " + currentHP);
+
+        StartCoroutine(InvincibleCoroutine());
 
         // HP0以下なら死亡
         if (currentHP <= 0)
@@ -34,7 +50,6 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
-    // Die:
     // HP0時処理
     private void Die()
     {
@@ -42,5 +57,22 @@ public class PlayerHealth : MonoBehaviour
 
         // 仮で非表示
         gameObject.SetActive(false);
+    }
+
+    // 一定時間無敵状態にする
+    private System.Collections.IEnumerator InvincibleCoroutine()
+    {
+        // 無敵ON
+        isInvincible = true;
+
+        Debug.Log("無敵開始");
+
+        // 指定秒待機
+        yield return new WaitForSeconds(invincibleTime);
+
+        // 無敵OFF
+        isInvincible = false;
+
+        Debug.Log("無敵時間終了");
     }
 }
